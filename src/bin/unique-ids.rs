b8196fe2e3070a3b1b98471a930ab98397e4bc8a
Jordi Serra Torrens <jor.serra.t@gmail.com>
2026-05-25 16:22:10 +0200

    unique-ids

--- src/bin/unique-ids.rs
+++ src/bin/unique-ids.rs
@@ -2,10 +2,12 @@ use flyio::Node;
 use serde_json::json;
 
 fn main() {
-    Node::run(|_node, msg_type, _body| match msg_type {
+    let mut counter = 0;
+    Node::run(|node: &mut flyio::Node, msg_type, _body| match msg_type {
         "generate" => {
-            // TODO: generate a unique ID
-            Some(json!({ "type": "generate_ok", "id": "TODO" }))
+            let unique_id = format!("{}-{}", node.id, counter);
+            counter += 1;
+            Some(json!({ "type": "generate_ok", "id": unique_id }))
         }
         other => {
             log::warn!("unknown message type: {other}");
