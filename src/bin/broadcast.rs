674c472932afecccf8114f8adf4114c8f1afc776
Jordi Serra Torrens <jor.serra.t@gmail.com>
2026-05-27 17:28:10 +0200

    broadcast pt 2

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -2,6 +2,12 @@ use flyio::Node;
 use serde_json::json;
 use std::collections::HashSet;
 
+fn broadcast_to_neighbors(node: &mut flyio::Node, neighbors: &Vec<String>, message: &serde_json::Value) {
+    for neighbor in neighbors {
+        node.send(neighbor, json!({ "type": "broadcast", "message": message }));
+    }
+}
+
 fn main() {
     let mut seen_messages = HashSet::new();
     let mut my_neighbors = Vec::new();
@@ -10,8 +16,10 @@ fn main() {
         |node: &mut flyio::Node, msg_type, body: &serde_json::Value| match msg_type {
             "broadcast" => {
                 let message = &body["message"];
-                seen_messages.insert(message.clone());
-                // TODO: Actually broadcast
+                let inserted = seen_messages.insert(message.clone());
+                if inserted {
+                    broadcast_to_neighbors(node, &my_neighbors, message);
+                }
                 Some(json!({ "type": "broadcast_ok"}))
             }
             "read" => {
@@ -26,6 +34,10 @@ fn main() {
                     .map(|x| x.as_str().unwrap().to_string())
                     .collect();
                 Some(json!({"type": "topology_ok"}))
+            }
+            "broadcast_ok" => {
+                // noop for now
+                None
             }
             other => {
                 log::warn!("unknown message type: {other}");
